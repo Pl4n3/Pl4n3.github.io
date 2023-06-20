67c92bff19ad127397e05da6ec19c9437a32ed7c
Pl4n3 <pl@jzone.de>
2023-06-20 19:24:07 +0200

    Add files via upload

--- util/deep.js
+++ util/deep.js
@@ -1,7 +1,7 @@
 //----
 var Deep=function(ps) {
   var rH=ps.rH?ps.rH:undefined,PI=Math.PI,
-      version='0.210 ',//FOLDORUPDATEVERSION
+      version='0.328 ',//FOLDORUPDATEVERSION
       newView=true,ovs=[],lastcurs=undefined,curso=undefined,
       self=this,dt=0;
   function dkey(x,y,z) {
@@ -15,6 +15,7 @@ var Deep=function(ps) {
   function getR(z,y,x,create) {
     var ret=rH[dkey(x,y,z)];
     if (create&&!ret) {
+      //console.log('deep.getR create '+x+' '+y+' '+z);console.trace();
       ret=initR(x,y,z);
     }
     return ret;
@@ -40,23 +41,45 @@ var Deep=function(ps) {
     return 1;
   }
   this.maxlen=2;
+  this.moveUpdown=true;
   this.pathStart=function(g,o) {
     if (g.gpos) if (!g.len||(g.gpos.len<g.len)) g=g.gpos;
     
     var path=[g],g0=g,g1;
+    let updown=!o.fly;
     while (true) {
       var gs=[],x=g0.x,y=g0.y,z=g0.z;
       //console.log(g0);
-      g1=getR(z-1,y,x);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); 
+      //onsole.log(x+' '+y+' '+z);
+      g1=getR(z-1,y,x);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); else if (updown) {
+      g1=getR(z-1,y+1,x);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); else {
+      g1=getR(z-1,y-1,x);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1);
+      }}
       //console.log(g1);console.log(gs.length);
-      g1=getR(z,y,x-1);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); 
+      g1=getR(z,y,x-1);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); else if (updown) {
+      g1=getR(z,y+1,x-1);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); else {
+      g1=getR(z,y-1,x-1);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); 
+      }}
       //console.log(g1);console.log(gs.length);
-      g1=getR(z+1,y,x);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); 
+      g1=getR(z+1,y,x);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); else if (updown) {
+      g1=getR(z+1,y+1,x);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); else {
+      g1=getR(z+1,y-1,x);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); 
+      }}
       //console.log(g1);console.log(gs.length);
-      g1=getR(z,y,x+1);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); 
-      //console.log(g1);console.log(gs.length);
+      g1=getR(z,y,x+1);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); else if (updown) {
+      g1=getR(z,y+1,x+1);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); else { 
+      g1=getR(z,y-1,x+1);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1); 
+      }}
+      
+      if (o.fly) {
+        g1=getR(z,y-1,x);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1);
+        g1=getR(z,y+1,x);if (g1&&(g1.len!==undefined)&&(g1.len<g0.len)) gs.push(g1);
+      }
+      
+      //console.log(g1);
+      //onsole.log(gs.length);
       if (gs.length==0) break;
-      g0=gs[Math.floor(Math.random()*gs.length)];
+      g0=gs[Conet.rani(gs.length)];//Math.floor(Math.random()*gs.length)];
       path.splice(0,0,//g0.gpos||
         g0);
       //path.push(g0.gpos||g0);
@@ -67,11 +90,13 @@ var Deep=function(ps) {
     for (var i=0;i<path.length;i++) {
       var g=path[i];//,x=g.x,y=g.y;
       //g.len=1;
-      var y=0;
-      for (var z=g.z;z<g.z+o.zw;z++) for (var x=g.x;x<g.x+o.xw;x++)
-        getR(z,y,x,1).len=1;
+      var y=g.y;
+      for (var z=g.z;z<g.z+o.zw;z++) for (var x=g.x;x<g.x+o.xw;x++) {
+        var g1=getR(z,y,x);
+        if (g1) g1.len=1;
+      }
     }
-    
+    //onsole.log(path);
     
     return path;
     //...
@@ -88,15 +113,29 @@ var Deep=function(ps) {
   this.calcLen=function(x,y,z,len,o) {
     var g=getR(z,y,x);
     
-    
+    //onsole.log('calcLen 0');
     if (g&&(g.len!=-1)&&(g.len<len)) return 0;
     
     //onsole.log('calcLen 1');
-    for (var zh=z;zh<(z+o.zw);zh++)
-    for (var yh=y;yh<(y+o.yw);yh++) 
-    for (var xh=x;xh<(x+o.xw);xh++) {
+    for (var zh=z;zh<(z+o.zw);zh++) for (var yh=y;yh<(y+o.yw);yh++) for (var xh=x;xh<(x+o.xw);xh++) {
       if (!freeFor(getR(zh,yh,xh),o)) return 0;
     }
+    
+    //onsole.log('calcLen 2');
+    
+    if (!o.fly) {
+    var ground=false;var g1;
+    for (var zh=z;zh<(z+o.zw);zh++) for (var xh=x;xh<(x+o.xw);xh++) {
+      if (!freeFor(g1=getR(zh,y-1,xh),o)) {
+        if (g1&&g1.os&&(g1.os.length>0)) { } else
+        ground=true;
+      }
+    }
+    if (!ground) return 0;
+    }
+    
+    //onsole.log('calcLen 3');
+    
     
     if (!g) { g=initR(x,y,z); }
     g.len=len;
@@ -114,28 +153,57 @@ var Deep=function(ps) {
     var ret=1;
     if (len>=this.maxlen) return ret;
     
-    ret+=this.calcLen(x-1,y,z,len,o);
-    ret+=this.calcLen(x,y,z-1,len,o);
-    ret+=this.calcLen(x+1,y,z,len,o);
-    ret+=this.calcLen(x,y,z+1,len,o);
+    //onsole.log('calcLen 4');
+    
+    
+    var updown=self.moveUpdown&&!o.fly;
+    
+    var r0;
+    r0=this.calcLen(x-1,y,z,len,o);if (updown) { if (r0==0) r0=this.calcLen(x-1,y+1,z,len,o);if (r0==0) r0=this.calcLen(x-1,y-1,z,len,o); }
+    ret+=r0;
+    //ret+=this.calcLen(x-1,y,z,len,o);
+    r0=this.calcLen(x+1,y,z,len,o);if (updown) { if (r0==0) r0=this.calcLen(x+1,y+1,z,len,o);if (r0==0) r0=this.calcLen(x+1,y-1,z,len,o); }
+    ret+=r0;
+    //ret+=this.calcLen(x+1,y,z,len,o);
+    r0=this.calcLen(x,y,z-1,len,o);if (updown) { if (r0==0) r0=this.calcLen(x,y+1,z-1,len,o);if (r0==0) r0=this.calcLen(x,y-1,z-1,len,o); }
+    ret+=r0;
+    //ret+=this.calcLen(x,y,z-1,len,o);
+    r0=this.calcLen(x,y,z+1,len,o);if (updown) { if (r0==0) r0=this.calcLen(x,y+1,z+1,len,o);if (r0==0) r0=this.calcLen(x,y-1,z+1,len,o); }
+    ret+=r0;
+    
+    if (o.fly) {
+      r0=this.calcLen(x,y-1,z,len,o);ret+=r0;
+      r0=this.calcLen(x,y+1,z,len,o);ret+=r0;
+    }
+    
+    //ret+=this.calcLen(x,y,z+1,len,o);
     return ret;
   }
-  this.checkMarkAttack=function(o,retArray) {
+  this.checkMarkAttack=function(o,retArray,ps) {
     
     var ar=o.ar||1,y=o.y,a=retArray?[]:undefined;//,ow=o.w||1;
-    for (var z=o.z-ar;z<o.z+o.zw+ar;z++) for (var x=o.x-ar;x<o.x+o.xw+ar;x++) {
+    for (var z=o.z-ar;z<o.z+o.zw+ar;z++) for (var x=o.x-ar;x<o.x+o.xw+ar;x++) 
+      for (var y=o.y-1;y<=o.y+1;y++)
+      {
       //if ((x<0)||(y<0)||(x>=gw)||(y>=gw)) continue;
       var dx=x<o.x?x-o.x:(x>o.x+o.xw-1?x-o.x-o.xw+1:0),
           dz=z<o.z?z-o.z:(z>o.z+o.zw-1?z-o.z-o.zw+1:0);
       if ((dx==0)&&(dz==0)) continue;
       //console.log(dx+' '+dy+' sum '+(Math.abs(dx)+Math.abs(dy)));
-      if ((Math.abs(dx)+Math.abs(dz))>ar) continue;
+      var d=Math.abs(dx)+Math.abs(dz);
+      if (d>ar) continue;
+      if (o.arMin) if (d<o.arMin) continue;
       var g=getR(z,y,x);//,1);
       if (!g) continue;
+      if (getR(z,y-1,x)) continue;//attack only on ground (for nao)
+      var o0=undefined;
       if (g.os) if (g.os.length>0) {
-        var o0=g.os[0];
+        o0=g.os[0];
         if (o0.party===o.party) continue;//no friendly fire
-      }
+        if (self.friendlyParty) if (self.friendlyParty(o0.party,o.party)) continue;
+        if (retArray&&!o0.isvis) continue;//cannot calc invisible objs
+      } 
+      if (!o0&&ps&&ps.withObjs) continue;
       if (g.block) continue;
       if (retArray) a.push(g); else g.mark=1;
     }
@@ -144,6 +212,9 @@ var Deep=function(ps) {
     //...
   }
   this.placeGrid=function(o,yes) {
+    //onsole.log('deep.placeGrid '+o.x+' '+o.y+' '+o.z+' '+yes);
+    //if (!yes) 
+    //return;
     for (var z=o.z+o.zw-1;z>=o.z;z--) 
     for (var y=o.y+o.yw-1;y>=o.y;y--) 
     for (var x=o.x+o.xw-1;x>=o.x;x--) {
@@ -204,14 +275,17 @@ var Deep=function(ps) {
     for (var oi=oaa.length-1;oi>=0;oi--) {
     var o=oaa[oi];
     if (o) {
-    var rad=o.viewlen||7,rad2=rad*rad,c=0,
+    var rad=o.viewlen||7,rad2=rad*rad,c=0,radym=o.viewlendown||(rad+3),eyeh=o.eyeh||1,
         x0=round(o.x),y0=round(o.y+(o.eyeh||1)),z0=round(o.z);//Math.floor(o.x+0.5),y0=Math.floor(o.y+0.5),z0=Math.floor(o.z+0.5);
     //onsole.log('deep.setView '+x0+' '+y0+' '+z0);
     var oa=o.a;
     if (o.roty!==undefined) oa=-o.roty+PI/2;
-    for (var zh=-rad;zh<=rad;zh++) for (var yh=-rad;yh<=rad;yh++) for (var xh=-rad;xh<=rad;xh++) {
+    for (var yh=-radym;yh<=rad;yh++) {
+    var rady=yh>=-eyeh?rad:radym,rady2=rady*rady;
+    for (var zh=-rady;zh<=rady;zh++) 
+    for (var xh=-rady;xh<=rady;xh++) {
       var l=zh*zh+xh*xh+yh*yh;
-      if (l>rad2) continue;
+      if (l>rady2) continue;
       var x1=x0+xh,y1=y0+yh,z1=z0+zh;
       var r=getR(z1,y1,x1);
       if (!r) continue;
@@ -243,7 +317,7 @@ var Deep=function(ps) {
     }
     //onsole.log('deep.setViews '+c);
     //onsole.log(o);
-    }
+    }}
     //onsole.log(oa);
     var change=false;
     if (vs.length!=ovs.length) change=true; else 
@@ -310,7 +384,7 @@ var Deep=function(ps) {
         var adda=Math.abs(dda);
         //if (adda<0.1) delete(o.adest);
         if (dda>0) o.da=1; else o.da=-1;
-        if (o.pdest) if (adda<0.5) o.d=o.v;
+        if (o.pdest||o.runOnA) if (adda<0.5) o.d=o.v;
       }
       if (o.da!=0) {
         var da=o.da*dt*0.005;//*0.5;
@@ -327,6 +401,7 @@ var Deep=function(ps) {
         o.running=true;
         if (o==curs) newView=true;
       }
+      var upstairs=false;
       if (o.d!=0) {
         var d=dt*o.d*(o.crouch?0.5:1),dx=d*Math.cos(o.a),dz=d*Math.sin(o.a),dzh=dz,dxh=dx,r=0.4;
         if (dzh<0) dzh-=r; else if (dzh>0) dzh+=r;
@@ -337,7 +412,12 @@ var Deep=function(ps) {
         //if (getR(round(o.z+dzh),round(o.y),round(o.x+dxh))) {
         
         if (!place(o,dxh,0,dzh)||!place(o,dx,0,dz)) {
-          if (place(o,dxh,0,0)) { dz=0;dzh=0; } else if (place(o,0,0,dzh)) { dx=0;dxh=0; }}
+          upstairs=place(o,dxh,1,dzh);
+          if (upstairs) o.jumpt=150;
+          //console.log(placeUp);
+          if (place(o,dxh,0,0)) { dz=0;dzh=0; } 
+          else if (place(o,0,0,dzh)) { dx=0;dxh=0; }
+        }
         if (place(o,dxh,0,dzh)) {
         
           o.x+=dx;o.z+=dz;
@@ -350,7 +430,9 @@ var Deep=function(ps) {
         }
       }
     
-      var dy=dt*(o.jumpt>0?0.01:-0.005);
+      var dy=dt*(((o.jumpt>0)
+        //||upstairs
+        )?0.01:-0.005);
       if (place(o,0,dy+(dy>0?0.5:-0.5),0)&&place(o,0,dy,0)) { //0.5 is similar to radius r above
         o.y+=dy;
         o.moving=true;
@@ -383,10 +465,47 @@ var Deep=function(ps) {
     return 0;
     //...
   }
+  this.dista=function(o0,o1) {
+    //---
+    var dx=o0.x-o1.x;
+    if (o1.xw) {
+      //onsole.log('deep.dist '+dx+' '+o1.xw);
+      if (dx>0) dx=Math.max(0,dx-(o1.xw-1));
+    }
+    var dy=o0.y-o1.y;
+    if (o1.yw) {
+      //onsole.log('deep.dist '+dy+' '+o1.yw);
+      if (dy>0) dy=Math.max(0,dy-(o1.yw-1));
+    }
+    var dz=o0.z-o1.z;
+    
+    if (o1.zw) {
+      //onsole.log('deep.dist '+dz+' '+o1.zw);
+      if (dz>0) dz=Math.max(0,dz-(o1.zw-1));
+    }
+    
+    return Math.abs(dx)+Math.abs(dy)+Math.abs(dz);
+    //...
+  }
   function dist(o0,o1) {
     var dx=o0.x-o1.x;
+    if (o1.xw) {
+      //onsole.log('deep.dist '+dx+' '+o1.xw);
+      if (dx>0) dx=Math.max(0,dx-(o1.xw-1));
+    }
     var dy=o0.y-o1.y;
+    if (o1.yw) {
+      //onsole.log('deep.dist '+dy+' '+o1.yw);
+      if (dy>0) dy=Math.max(0,dy-(o1.yw-1));
+    }
     var dz=o0.z-o1.z;
+    
+    if (o1.zw) {
+      //onsole.log('deep.dist '+dz+' '+o1.zw);
+      if (dz>0) dz=Math.max(0,dz-(o1.zw-1));
+    }
+    
+    
     return Math.sqrt(dx*dx+dy*dy+dz*dz);
   }
   function dAnglea(o,a) {
@@ -476,13 +595,52 @@ var Deep=function(ps) {
     for (var z=o.z+o.zw-1;z>=o.z;z--)
     for (var y=o.y+o.yw-1;y>=o.y;y--)
     for (var x=o.x+o.xw-1;x>=o.x;x--)
-      if (getR(z,y,x).view) return true;
+      if (getR(z,y,x).view) { o.isvis=true;return true; }
+    o.isvis=false;
     return false;
     //...
   }
+  
+  this.getFreePositions=function() {
+    //---
+    var pa=[];
+    for (var k in rH) {
+      var g=rH[k];
+      if (g.os&&g.os.length>0) continue;
+      if (getR(g.z,g.y-1,g.x)) continue;
+      if (g.sky) continue;
+      if (g.y!=0) continue;
+      pa.push(g);
+      //onsole.log(g);
+    }
+    return pa;
+    //...
+  }
+  this.setBlocks=function(blocks) {
+    //----
+    for (var i=0;i<blocks.length;i++) {
+      var b=blocks[i];
+      for (var z=b.z;z<b.z+b.zw;z++)
+      for (var y=b.y;y<b.y+b.yw;y++)
+      for (var x=b.x;x<b.x+b.xw;x++) {
+        if (b.i) {
+          delete rH[dkey(x,y,z)];
+          continue;
+        }
+        var g=getR(z,y,x,1);
+        //g.block=1;
+        if (b.dbg) g.dbg=1;
+        if (b.sky) g.sky=1;
+        //if (b.ground) g.ground=1;
+        g.wview=1;
+      }
+    }
+    //...
+  }
+  
   this.dist=dist;
   this.dAngle=dAngle;
-  this.rH=rH;
+  this.rH=rH;this.dkey=dkey;
   this.getR=getR;
   this.setViews=setViews;
   //----
@@ -490,7 +648,6 @@ var Deep=function(ps) {
 }
 
 //fr o,1
-//fr o,1,20
-//fr o,1,21
-//fr o,1,29
-//fr p,7,80
+//fr o,1,25
+//fr o,1,26
+//fr p,45,46

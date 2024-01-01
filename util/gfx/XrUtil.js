b99600ab6c7d22ede206057a76f3b9b73f7af6e2
Pl4n3 <pl@jzone.de>
2024-01-01 22:30:04 +0100

    Add files via upload

--- util/gfx/XrUtil.js
+++ util/gfx/XrUtil.js
@@ -3,7 +3,10 @@ import { XRControllerModelFactory } from '/three/r124/examples/jsm/webxr/XRContr
 let XrUtil={};
 (function(pself) {
   //---
-  let self=pself,gp0,camera,room,vrPos;
+  let version='v.1.128 ',//FOLDORUPDATEVERSION
+      self=pself,ctrl0,ctrl1,gp0,gp1,camera,room,vrPos,huds=[],hudMesh,
+      hud={lines:['xrUtil '+version],cursor:{x:0.5,y:0.5,vis:false},buttons:[]},
+      raycaster,INTERSECTED,hudCount=0;
   
   const tempMatrix=new THREE.Matrix4(),vt=new THREE.Vector3();
   
@@ -100,7 +103,7 @@ let XrUtil={};
     
     
     
-    let ctrl0=renderer.xr.getController(0);
+    ctrl0=renderer.xr.getController(0);
     ctrl0.addEventListener('selectstart',onSelectStart);
     ctrl0.addEventListener('selectend',onSelectEnd);
     
@@ -129,14 +132,14 @@ let XrUtil={};
     //cg0.add(controllerModelFactory.createControllerModel(cg0));
     //ps.scene.add(cg0);
     
-    let ctrl1=renderer.xr.getController(1);
+    ctrl1=renderer.xr.getController(1);
     ctrl1.addEventListener('selectstart',onSelectStart);
     ctrl1.addEventListener('selectend',onSelectEnd);
     if (!ps.sculpt)
     ctrl1.addEventListener('connected',function(e) {
       //---
       this.add(buildController(e.data));
-      self.gp1=e.data.gamepad;
+      gp1=e.data.gamepad;self.gp1=gp1;
       //onsole.log(XrU til.gp1);
       //onsole.log('ctrl1 connected');
       //...
@@ -191,7 +194,7 @@ let XrUtil={};
       //...
     }
     };
-    self.menuXr.ms='v.0.14 ';//FOLDORUPDATEVERSION
+    self.menuXr.ms=version;
     
     if ('xr' in navigator) {
       navigator.xr.isSessionSupported('immersive-ar')
@@ -210,19 +213,136 @@ let XrUtil={};
     //...
   }
   
+  function drawHud() {
+    //---
+    const ct=hud.ct,c=hud.c,w=c.width,h=c.height;
+    ct.clearRect(0,0,w,h);
+    ct.fillStyle='rgba(0,0,0,0.3)';ct.fillRect(0,0,w,h);
+    ct.font='20px sans-serif';//ct.textBaseline='top';
+    //ct.fillText('c='+hudCount,2,2);
+    const cur=hud.cursor,curx=cur.x*w,cury=cur.y*h;
+    ct.textAlign='center';
+    ct.textBaseline='middle';
+    for (let b of hud.buttons) {
+      const bx=b.x*w,by=b.y*h,bw=b.w*w,bh=b.h*h;
+      if (b.pressed&&(!cur.down||!cur.vis)) b.pressed=false;
+      if (cur.vis&&hudMesh.visible) {
+        if ((curx>=bx)&&(cury>=by)&&(curx<=bx+bw)&&(cury<=by+bh)) {
+          if (cur.down&&!b.pressed) {
+            b.pressed=true;
+            b.ondown();
+          }
+          ct.fillStyle=b.pressed?'rgba(150,150,50,0.5)':'rgba(100,100,100,0.5)';
+          ct.fillRect(bx,by,bw,bh);
+        }
+      } 
+      ct.strokeStyle='#aaa';
+      ct.strokeRect(bx,by,bw,bh);
+      ct.fillStyle='#ddd';
+      ct.fillText(b.s,bx+bw/2,by+bh/2);
+    }
+    ct.textAlign='start';
+    ct.textBaseline='top';
+    ct.fillStyle='#ddd';
+    ct.font='14px sans-serif';//ct.textBaseline='top';
+    for (let i=0;i<hud.lines.length;i++) {
+      ct.fillText(hud.lines[i],3,3+i*12);
+    }
+    if (cur.vis) {
+      ct.strokeStyle='#fff';
+      ct.strokeRect(curx-5,cury-5,10,10);
+    }
+    hud.t.needsUpdate=true;
+    //...
+  }
   
-  console.log('XrUtil v.1.106 ');//FOLDORUPDATEVERSION
+  self.log=function(s) {
+    //---
+    hud.lines.push(s);
+    while (hud.lines.length>4) hud.lines.splice(0,1);
+    drawHud();
+    //...
+  }
+  
+  self.initHud=function(ps) {
+    const g=new THREE.PlaneGeometry(0.15,0.15);
+    const c=document.createElement('canvas');c.width=256;c.height=256;
+    const ct=c.getContext('2d');hud.c=c;hud.ct=ct;
+    //ct.fillStyle='rgba(0,0,0,0.3)';ct.fillRect(0,0,c.width,c.height);
+    //ct.font='20px sans-serif';ct.textBaseline='top';ct.fillStyle='#ff0';ct.fillText('n/i',2,2);
+    const t1=new THREE.Texture(c);hud.t=t1;
+    //t1.needsUpdate=true;
+    drawHud();
+    const planeMaterial=new THREE.MeshBasicMaterial({map:t1,opacity:1,transparent:true});
+    const o=new THREE.Mesh(g,planeMaterial);
+    o.position.set(-0.2,0,-0.5);
+    o.rotation.y=0.3;hudMesh=o;
+    camera.add(o);huds.push(o);
+    raycaster=new THREE.Raycaster();
+    
+    self.huds=huds;self.hudMesh=hudMesh;self.hud=hud;
+    //...
+  }
+  
+  self.renderHud=function() {
+    //---
+    if (hudMesh.visible) /* intersect */ {
+      // find intersections
+      tempMatrix.identity().extractRotation(ctrl1.matrixWorld);
+      raycaster.ray.origin.setFromMatrixPosition(ctrl1.matrixWorld);
+      raycaster.ray.direction.set(0,0,-1).applyMatrix4(tempMatrix);
+      
+      const intersects=raycaster.intersectObjects(huds),//room.children);
+            cursor=self.cursor;
+      if (intersects.length>0) {
+        const i0=intersects[0];
+        //console.log(i0);
+        if (INTERSECTED!=i0.object) {
+          //if (INTERSECTED) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+          INTERSECTED=i0.object;
+          //INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+          //INTERSECTED.material.emissive.setHex( 0xff0000 );
+          //onsole.log('intersected');
+          hudCount++;    
+        }
+        //hud.lines[0]='c '+hudCount;
+        //hud.lines[1]=i0.uv.x;
+        //hud.lines[2]=i0.uv.y;
+        hud.cursor.x=i0.uv.x;
+        hud.cursor.y=1-i0.uv.y;
+        hud.cursor.vis=true;
+        hud.cursor.down=gp1&&gp1.buttons[0].pressed;
+        drawHud();
+        if (cursor) cursor.visible=false;
+      } else {
+        //if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+        INTERSECTED=undefined;
+        if (cursor) cursor.visible=true;
+        if (hud.cursor.vis) {
+          hud.cursor.vis=false;
+          drawHud();
+        }
+      }
+    }
+    //...
+  }
+  
+  console.log('XrUtil '+version);
   //...
 }
 )(XrUtil);
 export { XrUtil };
 //fr o,3
-//fr o,3,6
-//fr o,3,7
-//fr o,3,7,7
-//fr o,3,7,9
-//fr o,3,7,79
-//fr o,3,7,80
-//fr o,3,7,82
-//fr o,3,7,88
-//fr p,7,128
+//fr o,3,10
+//fr o,3,10,7
+//fr o,3,10,9
+//fr o,3,10,56
+//fr o,3,10,79
+//fr o,3,10,80
+//fr o,3,10,82
+//fr o,3,10,88
+//fr o,3,14
+//fr o,3,16
+//fr o,3,18
+//fr o,3,18,1
+//fr p,23,137

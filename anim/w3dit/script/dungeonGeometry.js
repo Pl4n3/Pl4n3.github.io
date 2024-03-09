d6e76aa9f22c4b7d2df33274062e1ba9ca5e2cee
Pl4n3 <pl@jzone.de>
2024-03-09 12:19:01 +0100

    Add files via upload

--- anim/w3dit/script/dungeonGeometry.js
+++ anim/w3dit/script/dungeonGeometry.js
@@ -1,1 +1,21 @@
-123
+//----
+(function() {
+  //---
+  //onsole.log('dungeonGeometry.js');
+  //onsole.log(document.currentScript);
+  window.w3ditScriptInit(function (ps) {
+    //---
+    let m=new THREE.Mesh(
+       new THREE.BoxGeometry(0.1,0.1,0.1),
+       new THREE.MeshPhongMaterial({flatShading:true,color:0xdddddd,transparent:true,opacity:0.9}));
+    ps.mesh.add(m);
+    //...
+  }
+  );
+  //...
+}
+)();
+//----
+//fr o,1
+//fr o,1,3
+//fr p,3,10

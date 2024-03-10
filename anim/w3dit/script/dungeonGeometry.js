c8fb72b6671eca7c39a76c482b31131d33239efd
Pl4n3 <pl@jzone.de>
2024-03-10 12:43:51 +0100

    Add files via upload

--- anim/w3dit/script/dungeonGeometry.js
+++ anim/w3dit/script/dungeonGeometry.js
@@ -5,10 +5,41 @@
   //onsole.log(document.currentScript);
   window.w3ditScriptInit(function (ps) {
     //---
-    let m=new THREE.Mesh(
-       new THREE.BoxGeometry(0.1,0.1,0.1),
-       new THREE.MeshPhongMaterial({flatShading:true,color:0xdddddd,transparent:true,opacity:0.9}));
-    ps.mesh.add(m);
+    //let m=new THREE.Mesh(
+    //   new THREE.BoxGeometry(0.1,0.1,0.1),
+    //   new THREE.MeshPhongMaterial({flatShading:true,color:0xdddddd,transparent:true,opacity:0.9}));
+    //ps.mesh.add(m);
+    
+    let deep=new Deep({});
+    //onsole.log(deep);
+    Conet.download({fn:'/canvas/deep/deep7.txt',f:function(v) {
+      //---
+      let du=deep.dungeonLoad(JSON.parse('['+v+']'));
+      //onsole.log(du);
+      
+      let m1=new THREE.MeshPhongMaterial({color:0x77dd77,flatShading:true
+        ,transparent:true,opacity:0.5,depthWrite:false
+        });
+      
+      threeEnv.path='/shooter/';
+      let mesh;
+      threeSetMeshMaterial(mesh={diff:'objs/mapGen/d10.jpg',spec:'objs/mapGen/s1.jpg',norm:'objs/mapGen/n1.jpg'},{});
+      let m2=mesh.material;
+      //onsole.log(m2);
+      
+      let dum0=new THREE.Mesh(new THREE.BoxGeometry(0.1,0.1,0.1),m1);ps.mesh.add(dum0);
+      let dum1=new THREE.Mesh(new THREE.BoxGeometry(0.1,0.1,0.1),m2);ps.mesh.add(dum1);
+      //dum0.position.x=-0.2;dum1.position.x=0.2;
+      
+      du.allwview=1;//mviewall.checked;
+      dum0.geometry=threeEnv.dungeonGeometry(du);
+      dum1.geometry=threeEnv.dungeonGeometry(du,true);
+      let sc=0.001;
+      dum0.scale.set(sc,sc,sc);
+      dum1.scale.set(sc,sc,sc);
+      //...
+    }
+    });
     //...
   }
   );
@@ -18,4 +49,5 @@
 //----
 //fr o,1
 //fr o,1,3
-//fr p,3,10
+//fr o,1,3,8
+//fr p,11,36
